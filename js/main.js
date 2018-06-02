4d8407fb4d145a8d44f0229f365b088eb6b93e83
Smit Patadiya <smit.soni@gmail.com>
2018-06-03 02:19:38 +0530

    Added API to generate ETH Wallet

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
+(function ($) {
 
+    $(document).ready(function () {
+
+        $('#generate').on('click', function (event) {
+            event.preventdefault;
+
+            //Your Block Cypher token here
+            let token = '<Your Blockcypher token here>';
+
+            let args = {}
+            $.post('https://api.blockcypher.com/v1/eth/main/addrs?token=' + token, JSON.stringify(args))
+                .then(function (data) {
+
+                    // console.log(data);
+                    let eth_address, private_key, public_key;
+
+                    eth_address = '0x' + data.address;
+                    public_key  = data.public;
+                    private_key = data.private;
+
+                    $('#eth-address').val(eth_address);
+                    $('#pub-key').val(public_key);
+                    $('#private-key').val(private_key);
+
+
+
+                });
+        });
+
+    });
+    
+
+})(jQuery)
